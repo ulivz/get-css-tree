b52ee4c40d660e712c39d15c17d1b9a05329b66a
ulivz <472590061@qq.com>
2018-01-22 01:02:04 +0800

    test: fix

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,9 @@
 import fs from 'fs'
 import path from 'path'
-import cssTreeGetter from '../'
+import htmlCssTransformer from '../src'
 
-test('should get correct css tree', () => {
+test('should get correct css output', () => {
   const htmlstring = fs.readFileSync(path.resolve(__dirname, '__fixtures__/test.html'), 'utf-8')
-  const csstree = cssTreeGetter(htmlstring)
+  const csstree = htmlCssTransformer(htmlstring)
   expect(csstree).toMatchSnapshot()
 })
